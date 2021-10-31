ee38e9ebc8b59b348877e772745fca50fc69f5e7
Peter Knipp <peteraknipp@gmail.com>
2021-10-31 07:58:16 -0400

    Implement and test ludcmp.py and lubksb.py (ie, solving the matrix equation ax = b).

---
+++ matrix_inverse/__init__.py
@@ -0,0 +1,38 @@
+import cmath
+from flask import Flask
+from . import helper
+
+app = Flask(__name__)
+
+# The following are used to wrap the html string created for server-side rendering.
+top = "<head><title>Matrix inverse</title></head><body>"
+bottom = "<span>creator:&nbsp;<a href='https://pknipp.github.io/' target='_blank' rel='noopener noreferrer'>Peter Knipp</a></span></body>"
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def react_root(path):
+    print("path", path)
+    if path == 'favicon.ico':
+        return app.send_static_file('favicon.ico')
+    return app.send_static_file('index.html')
+
+
+@app.route('/')
+def hello():
+    html = top + "<h3><p align=center>Instructions:</p></h3>"
+    return html
+
+@app.route('/<str_in>')
+def return_html(str_in):
+    # results = helper.parse_roots(str_in, False)
+    results = '<h1>Hello world</h1>'
+    if isinstance(results, str):
+        return top + results + bottom
+    else:
+        return '<h1>' + results["error"] + '</h1>'
+
+@app.route('/json/<str_in>')
+def return_json(str_in):
+    # return helper.parse_roots(str_in, True)
+    return {"message": "Hello world"}
