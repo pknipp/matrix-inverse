45042fdc8e7cab30f51e4d08de8bab28cc658818
Peter Knipp <peteraknipp@gmail.com>
2024-10-22 09:23:53 -0400

    Add a space in order to create an empty commit.

--- matrix_inverse/__init__.py
+++ matrix_inverse/__init__.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 # The following are used to wrap the html string created for server-side rendering.
 top = "<head><title>Matrix inverse</title></head><body>"
-bottom = "<p align=center>creator:&nbsp;<a href='https://pknipp.github.io/' target='_blank' rel='noopener noreferrer'>Peter Knipp</a><br/>repo:&nbsp;<a href='https://github.com/pknipp/matrix-inverse' target='_blank' rel='noopener noreferrer'>github.com/pknipp/matrix-inverse</a></p></body>"
+bottom = "<p align=center>creator:&nbsp;<a href='https://pknipp.github.io/' target='_blank' rel='noopener noreferrer'>Peter Knipp</a><br/>repo:&nbsp;<a href='https://github.com/pknipp/matrix-inverse' target='_blank'  rel='noopener noreferrer'>github.com/pknipp/matrix-inverse</a></p></body>"
 
 
 @app.route('/', defaults={'path': ''})
